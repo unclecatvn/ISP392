9a9e37684a118a5ea200350e01dcf40d5206f7b1
Hoai Nam <nguyenhoain123@gmail.com>
2023-09-19 07:53:32 +0700

    Add files via upload

---
+++ QuizTaking/src/java/LoginGoogleServlet.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+//@WebServlet("/login-google")
+public class LoginGoogleServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public LoginGoogleServlet() {
+        super();
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         String code = request.getParameter("code");
+    if (code == null || code.isEmpty()) {
+      RequestDispatcher dis = request.getRequestDispatcher("login.jsp");
+      dis.forward(request, response);
+    } else {
+      String accessToken = GoogleUtils.getToken(code);
+      GooglePojo googlePojo = GoogleUtils.getUserInfo(accessToken);
+      request.setAttribute("id", googlePojo.getId());
+      request.setAttribute("name", googlePojo.getName());
+      request.setAttribute("email", googlePojo.getEmail());
+      RequestDispatcher dis = request.getRequestDispatcher("index.jsp");
+      dis.forward(request, response);
+    }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ QuizTaking/web/Login.jsp
@@ -0,0 +1,19 @@
+<%-- 
+    Document   : Login
+    Created on : Sep 18, 2023, 10:24:01 AM
+    Author     : Acer
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>login</h1>
+  <a href="https://accounts.google.com/o/oauth2/auth?scope=email&redirect_uri=http://localhost:8080/QuizTaking/login-google&response_type=code
+    &client_id=17343589046-105nt6idiu9esmhfthjkun5jdrlvafcv.apps.googleusercontent.com&approval_prompt=force">Login With Google</a>  
+    </body>
+</html>
